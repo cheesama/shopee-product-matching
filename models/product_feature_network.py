08b090c64da52bd5005d3814e76e99713690f167
chee_mac <chee_mac@chee-macui-Macmini.local>
2021-04-23 22:54:19 +0900

    add bias to feature layer for better performance

--- models/product_feature_network.py
+++ models/product_feature_network.py
@@ -23,10 +23,10 @@ class ProductFeatureNet(nn.Module):
 
         self.feature_dim = feature_dim
         self.backbone_net = eval(backbone_net)(pretrained=True)
-        self.feature_layer = nn.Linear(
-            self.backbone_net.fc.out_features, feature_dim, bias=False
-        )
-        nn.init.xavier_uniform_(self.feature_layer.weight)
+
+        self.feature_layer = nn.Linear(self.backbone_net.fc.out_features, feature_dim)
+        nn.init.kaiming_uniform_(self.feature_layer.weight)
+        nn.init.kaiming_uniform_(self.feature_layer.bias)
 
     def forward(self, images):
         features = self.backbone_net(images)
