6b63a2309e79fb6b91af79366f8813e3d5b593b2
chee_mac <chee_mac@chee-macui-Macmini.local>
2021-04-26 01:55:08 +0900

    add text features 7

--- data_loader/pair_dataset.py
+++ data_loader/pair_dataset.py
@@ -30,8 +30,8 @@ class ProductPairDataset(Dataset):
 
         self.text_features = None
         for i in tqdm(range(len(self.products_frame) // self.batch_size + 1), desc='preparing text features in advance ...'):
-            extracted_features = text_feature_extractor(list(self.products_frame['title'])[(i * self.batch_size):((i + 1) * self.batch_size)])[:,0,:]
-            extracted_features = torch.FloatTensor(extracted_features)
+            extracted_features = text_feature_extractor(list(self.products_frame['title'])[(i * self.batch_size):((i + 1) * self.batch_size)])
+            extracted_features = torch.FloatTensor(extracted_features)[:,0,:]
             if self.text_features is None:
                 self.text_features = extracted_features
             else:
